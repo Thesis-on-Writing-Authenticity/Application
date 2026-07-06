126816fbfff9ecc0e904554cc43b736d3e188f27
Ville-A <ville.m.ahola@gmail.com>
2026-07-06 15:25:30 +0300

    Made the extension ready for React + Vite

---
+++ extension/src/popup/App.jsx
@@ -0,0 +1,9 @@
+import { useState } from 'react'
+
+export default function App() {
+  return (
+    <div style={{ width: 200, padding: 16, fontFamily: 'sans-serif' }}>
+      <h3 style={{ margin: '0 0 8px' }}>Extension</h3>
+    </div>
+  )
+}
